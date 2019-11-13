9e73ceb6ba7f08fae02d3159cb15cbf4485310ad
Default user <sm.morelli@uniandes.edu.co>
2019-11-13 20:17:46 +0000

    Clase_14.cpp

---
+++ Clase_14.cpp
@@ -0,0 +1,45 @@
+//Este código implementa el método de Euler para ecuaciones diferenciales ordinarias de primer orden.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Declaración Variables.
+
+double y0 = 0; 
+double t0 = 0; 
+double h0 = 0.01;
+
+//Declaración Funciones.
+
+double funct(double PosY, double t);
+    
+double Euler(double PosY, double t, double h);
+
+//Función Main.
+
+int main()
+{
+    cout<<t0<<", "<<y0<<endl;
+    
+    for(int i = 0; i <= 100; i++)
+    {
+        t0 = t0 + h0;
+        y0 = Euler(y0, t0, h0);
+        
+        cout<<t0<<", "<<Euler(y0, t0, h0)<<endl;
+    }
+    
+    return 0;
+}
+
+//Implementación funciones.
+
+double funct(double PosY, double t)
+{
+    return sin(t);
+}
+
+double Euler(double PosY, double t, double h)
+{
+    return PosY + (h*funct(PosY, t));
+}
